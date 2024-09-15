038983b5150768ea2b104bd424bba651c6f21c0c
Artem Boyko <artemboyko43@gmail.com>
2024-09-15 16:07:34 +0000

    Added functionality for saving transactions to firestore

--- src/main/java/de/rwth/idsg/steve/service/FirebaseService.java
+++ src/main/java/de/rwth/idsg/steve/service/FirebaseService.java
@@ -5,6 +5,7 @@ import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.*;
 import com.google.firebase.cloud.FirestoreClient;
 import de.rwth.idsg.steve.SteveConfiguration;
+import de.rwth.idsg.steve.repository.ChargePointRepository;
 import de.rwth.idsg.steve.repository.TransactionRepository;
 import de.rwth.idsg.steve.repository.dto.InsertTransactionParams;
 import de.rwth.idsg.steve.repository.dto.TransactionDetails;
@@ -30,6 +31,8 @@ import static de.rwth.idsg.steve.SteveConfiguration.CONFIG;
 @Slf4j
 @Service
 public class FirebaseService {
+
+    @Autowired private ChargePointRepository chargePointRepository;
 
     @Autowired private TransactionRepository transactionRepository;
 
@@ -123,6 +126,53 @@ public class FirebaseService {
 
     public void endTransaction(UpdateTransactionParams params) {
         TransactionDetails transactionDetails = transactionRepository.getDetails(params.getTransactionId());
+        System.out.println("Firestore endTransaction step 1");
+        Map<String, Object> transactionData = new HashMap<>();
+
+        Double connectorPriceSource = (Double) chargePointRepository.getChargePointPrices(params.getChargeBoxId())
+                .get(transactionDetails.getTransaction().getConnectorId() - 1);
+        Float connectorPrice = connectorPriceSource.floatValue();
+
+        if (transactionDetails.getTransaction().getConnectorId() == 1) {
+            transactionData.put("connector1Price", connectorPrice);
+        }
+        if (transactionDetails.getTransaction().getConnectorId() == 2) {
+            transactionData.put("connector2Price", connectorPrice);
+        }
+        if (transactionDetails.getTransaction().getConnectorId() == 3) {
+            transactionData.put("connector3Price", connectorPrice);
+        }
+
+        Float amount = -(Float.parseFloat(transactionDetails.getTransaction().getStopValue()) / 1000) * connectorPrice;
+
+        transactionData.put("chargeBoxId", params.getChargeBoxId());
+        transactionData.put("ocppIdTag", transactionDetails.getTransaction().getOcppIdTag());
+        transactionData.put("connectorId", transactionDetails.getTransaction().getConnectorId());
+
+        transactionData.put("startTimestamp", transactionDetails.getTransaction().getStartTimestamp().toDate());
+        transactionData.put("stopTimestamp", params.getStopTimestamp().toDate());
+        transactionData.put("dateTime", params.getStopTimestamp().toDate());
+
+        transactionData.put("startValue", transactionDetails.getTransaction().getStartValue());
+        transactionData.put("stopValue", transactionDetails.getTransaction().getStopValue());
+        transactionData.put("stopReason", transactionDetails.getTransaction().getStopReason());
+        transactionData.put("stopEventActor", transactionDetails.getTransaction().getStopEventActor());
+        transactionData.put("amount", amount);
+
+        transactionData.put("id", params.getTransactionId());
+
+
+        System.out.println("Firestore endTransaction step 2");
+        System.out.println("Firestore endTransaction step: transaction that we wan to stop" + String.valueOf(params.getTransactionId()));
+
+        ApiFuture<WriteResult> future = firestore
+                .collection("transactions")
+                .document(
+                        String.valueOf(params.getTransactionId())
+                )
+                .set(transactionData);
+
+        System.out.println("Firestore endTransaction step 3");
 
         firestore
             .collection("active_transactions")
